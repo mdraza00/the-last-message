18573b7c56428bbd8c017697cbaa3fe7cbb5f0e8
mdraza00 <108754498+mdraza00@users.noreply.github.com>
2022-09-17 13:29:49 +0530

    complete

--- forMahinoor.js
+++ forMahinoor.js
@@ -1,22 +1,29 @@
 var msg = 0;
 $("#NEXT").click(function() {
   msg++;
-  if (msg == 1) {
+  if (msg === 1) {
     $("#cir").fadeOut();
     setTimeout(function() {
       $("#prlgm").removeClass("hide").fadeIn();
     }, 300);
 
   }
-  if( msg == 2){
+  if( msg === 2){
     $("#prlgm").fadeOut();
-    $("#NEXT").addClass("hide");
     setTimeout(function(){
       $("#dmd").removeClass("hide").fadeIn();
+    }, 400);
+  }
+  if(msg === 3){
+    $("#dmd").fadeOut();
+    $("#NEXT").addClass("hide");
+    setTimeout(function(){
+      $("#last-message").removeClass("hide").fadeIn();
     }, 400);
   }
 });
 setTimeout(function() {
   $("#prlgm").fadeOut();
   $("#dmd").fadeOut();
+  $("#last-message").fadeOut();
 }, 1);
