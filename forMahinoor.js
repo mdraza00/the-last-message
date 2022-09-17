5a44705d37d91a5f33fe07174ca9d15de43022d1
mdraza00 <108754498+mdraza00@users.noreply.github.com>
2022-09-17 11:47:28 +0530

    aaaa

--- forMahinoor.js
+++ forMahinoor.js
@@ -1,7 +1,22 @@
-var msg= 0;
-$("#NEXT").click(function(){
+var msg = 0;
+$("#NEXT").click(function() {
   msg++;
-  if(msg==1){
-  $("#cir").fadeOut();  
+  if (msg == 1) {
+    $("#cir").fadeOut();
+    setTimeout(function() {
+      $("#prlgm").removeClass("hide").fadeIn();
+    }, 300);
+
+  }
+  if( msg == 2){
+    $("#prlgm").fadeOut();
+    $("#NEXT").addClass("hide");
+    setTimeout(function(){
+      $("#dmd").removeClass("hide").fadeIn();
+    }, 400);
   }
 });
+setTimeout(function() {
+  $("#prlgm").fadeOut();
+  $("#dmd").fadeOut();
+}, 1);
